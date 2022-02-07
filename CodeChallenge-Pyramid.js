8fb875a77ccc3a06ce57c74267800873cd27c70e
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-07 09:18:26 -0600

    solved pyramid challenge

---
+++ CodeChallenge-Pyramid.js
@@ -0,0 +1,16 @@
+let pyramidBuilder = (rows) => {
+  for (let i = 1; i <= rows; i++) {
+    let str = "";
+    for (let k = 1; k <= rows - i; k++) {
+      str += " ";
+    }
+    for (let j = 0; j != 2 * i - 1; j++) {
+      str += "*";
+    }
+    for (let k = i + 1; k <= rows; k++) {
+      str += " ";
+    }
+    console.log(str);
+  }
+};
+pyramidBuilder(3);
